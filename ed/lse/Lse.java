a0b706b4c8080b006ecd334887f55449ab46fae9
IagoNog <iagonogueira227@gmail.com>
2017-04-06 01:47:16 -0300

    Lista S. Encadeada: Início e Fim, recursivo

--- ed/lse/Lse.java
+++ ed/lse/Lse.java
@@ -5,9 +5,6 @@
  */
 package projeto2;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 /**
  *
  * @author Iago Nogueira <iagonogueira227@gmail.com>
@@ -26,11 +23,16 @@ public class Lse {
     public void addInicio(int item) { //constante
         
         No node = new No(item);
-        node.setProx(inicio);
-        this.inicio = node;
-        this.fim = this.inicio;
+        if (this.size == 0) {
+            node.setProx(inicio);
+            this.inicio = node;
+            this.fim = this.inicio;
+            this.size++;
+        } else {
+            
+        }
         
-        this.size++;
+        
     }  
     
     public void addFim(int item) { //constante
@@ -46,7 +48,9 @@ public class Lse {
         }        
     }
     
-    public void add(int item, int pos) { //linear
+    public void add(int pos, int item) { //linear
+        No node = new No(item);
+        No node_aux = this.inicio;
         
         if (pos == 0) {
             this.addInicio(item);
@@ -54,20 +58,17 @@ public class Lse {
         } else if (pos == this.size) {
             this.addFim(item);
             
-        } else if (pos > 0 && pos < this.size) {
+        } else if (pos < 0 && pos > this.size) {
             System.out.println("Posição inválida");
             
         } else {
-            No node = new No(item);
-            No node_aux = this.inicio;
-            int cont = 0;
             
-            while (cont < (pos-1)) {
-                cont++;
+            for (int i = 0; i < pos-1; i++) {
                 node_aux = node_aux.getProx();
             }
             
             node.setProx(node_aux.getProx());
+            node_aux.setProx(node);
             this.size++;
         }
     }
@@ -76,6 +77,7 @@ public class Lse {
         
         No node_aux = this.inicio;
         this.inicio = node_aux.getProx();
+        this.size--;
         
         return node_aux.getItem();
     }
@@ -86,6 +88,7 @@ public class Lse {
         No x;
         x = this.fim;
         this.fim = null;
+        this.size--;
         
         return x.getItem();
     }
@@ -103,28 +106,28 @@ public class Lse {
         } else {
             No node_aux = this.inicio;
             No x;
-            int cont = 0;
             
-            while (cont <= pos--) {
-                cont++;
+            for (int i = 0; i < pos--; i++) {
                 node_aux = node_aux.getProx();
             }
             x = node_aux;
             node_aux.setProx(node_aux.getProx().getProx());
+            this.size--;
         }
     }
     
     public int size() { //constante
         return this.size;
     }
-
-    public void print() {
+    
+    public void print() { //constante
 	this.imprime(this.inicio);
-    }
-	
-    private void imprime(No n) {
-	if (n.getProx() != null)
-		imprime(n.getProx());
-		System.out.println(n.getConteudo());
 	}
+	
+    private void imprime(No n) { //constante
+        if (n.getProx() != null) {
+            imprime(n.getProx());
+            System.out.println(n.getItem());
+        }
+    }
 }
